27bdf3a6b59141b8de4a82d83192afeb75466193
themsdems <thomasszpk28@gmail.com>
2025-11-06 14:34:31 +0100

    Commit

---
+++ equipe/Elu.java
@@ -0,0 +1,5 @@
+public class Elu extends Personne {
+    public Elu(String nom, String prenom, int age) {
+        super(nom, prenom, age);
+    }
+}
---
+++ equipe/EquipeMunicipale.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class EquipeMunicipale {
+    List<Expert> experts;
+    Elu elu;
+    List<Evaluateur> evaluateurs;
+}
